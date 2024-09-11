e00b784d938d9b122971feaacd038d5b5b9521ef
Kevin <kparra2023@alu.uct.cl>
2024-09-10 21:18:14 -0300

    session_status para checkear el estado de sesion

---
+++ estructura/session_status.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+// Ejemplo
+// fetch(sessionCheckURL)
+// 	.then((r) => r.json())
+// 	.then((data) => {
+// 		if (data.status == "logged_out") return;
+// 		if (data.status == "logged_in") alert(data.name);
+// 	});
+
+if (isset($_SESSION['correo'])) {
+    echo json_encode([
+        'status' => 'logged_in',
+        'correo' => $_SESSION['correo'],
+        'nombre' => $_SESSION['nombre'],
+        'direccion' => $_SESSION['direccion'],
+        'telefono' => $_SESSION['telefono'],
+        'tipo_usuario' => $_SESSION['tipo_usuario']
+    ]);
+} else {
+    echo json_encode(['status' => 'logged_out']);
+}
+?>
